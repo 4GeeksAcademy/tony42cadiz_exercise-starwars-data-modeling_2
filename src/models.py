22820088f59327fb8d37327f11850b642f7d145d
ANTONIO MANUEL MOLINA CAÑETE <MOLFERGUELSA@HOTMAIL.COM>
2025-02-12 07:54:21 +0000

    Exercise-Starwars-data-modeling_2

--- src/models.py
+++ src/models.py
@@ -1,33 +1,58 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
-from sqlalchemy import create_engine, String, ForeignKey
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favorites = relationship('Favorite', backref='user')    
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    favorites = relationship('Favorite', backref='planet')
+
 class Person(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped["Address"] = relationship(back_populates="person")
-
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
-    person_id: Mapped[int] = mapped_column(ForeignKey("person.id"))
-    person: Mapped["Person"] = relationship(back_populates="address")
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    hair_color=Column(String(250), nullable=True)
+    gender=Column(String(250), nullable=True)
+    birth_year=Column(String(250), nullable=False)
+    favorites = relationship('Favorite', backref='person')
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    passengers = Column(String(250), nullable=False)
+    max_atmosphering_speed = Column(String(250), nullable=False)
+    favorites = relationship('Favorite', backref='starship')    
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    favoriteStarshipId = Column(Integer, ForeignKey('starship.id'), nullable=True)
+    favoritePersonId = Column(Integer, ForeignKey('person.id'), nullable=True)
+    favoritePlanetId = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    user = relationship('User', backref='favorites')
+    starship = relationship('Starship', backref='favorites')
+    person = relationship('Person', backref='favorites')
+    planet = relationship('Planet', backref='favorites')
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
